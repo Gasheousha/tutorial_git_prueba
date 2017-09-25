efcdf45e6b332a3eb9cdd55cbbb645be853a4267
Gasheousha <dhernandez@dfuls.cl>
2017-09-25 10:55:26 -0300

    Se agrega opcion de ingresar entero

--- programa.py
+++ programa.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 #-*-coding:utf8-*
-print('Hola Mundo!!!'):
+print('Hola Mundo!!!')
 n = 2
 k=0
 for i in range(1,n+1):
 	k += 1
 print("Se realizaron ",k," iteraciones")
+a = int(input("Ingrese entero: "))
+print("El entero ingresado +2 es igual a : ",a+2)
+print("Bye")
